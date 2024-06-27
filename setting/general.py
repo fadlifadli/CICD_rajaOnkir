0ce61fa17f481f167a9ed201ff3206409d342c86
fadlifadli <fadli.azhari.elka@gmail.com>
2024-06-27 13:36:00 +0700

    notif-Slack

--- setting/general.py
+++ setting/general.py
@@ -3,4 +3,5 @@ import os
 
 max_latency = 1000000
 api_key = "a6cd9638ef1b2f65ff3486ba2839c081"
-webhook_slack = os.environ.get('PYTEST_WEBHOOK_SLACK')
+webhook_slack = "https://hooks.slack.com/services/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"
+# webhook_slack = os.environ.get('PYTEST_WEBHOOK_SLACK')
